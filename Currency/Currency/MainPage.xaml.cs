c6eef1c59c9aaca35baabc4309d533cb09454d9c
Adithya Kamath <adithyakamath96@gmail.com>
2019-05-03 03:49:50 +0530

    Initial commit. Conversion complete. Layout left to complete

--- Currency/Currency/MainPage.xaml.cs
+++ Currency/Currency/MainPage.xaml.cs
@@ -13,5 +13,16 @@ namespace Currency
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Double currencyInDollars = Double.Parse(dollarCurrency.Text);
+
+            Double currencyInRupees = currencyInDollars * 69.79;
+
+            
+            convertedCurrency.Text = currencyInRupees.ToString();
+
+        }
     }
 }
